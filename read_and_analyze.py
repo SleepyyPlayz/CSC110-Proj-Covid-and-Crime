2fd898e0ed3e3d4fc809e08dcf8836cdbe8385b6
Nicholas Poon <47435386+nicholaspoon03@users.noreply.github.com>
2021-12-04 22:38:35 -0500

    creating more helper functions

--- read_and_analyze.py
+++ read_and_analyze.py
@@ -1,6 +1,10 @@
 import csv
 import datetime
 
+PROV_AND_TERR = {'British Columbia', 'Alberta', 'Saskatchewan', 'Manitoba', 'Ontario', 'Quebec',
+                 'Newfoundland & Labrador', 'New Brunswick', 'Nova Scotia', 'Prince Edward Island',
+                 'Northwest Territories', 'Nunavut', 'Yukon'}
+
 
 class EmergencyCall:
     """A data type representing the specific 911 call.
@@ -8,13 +12,13 @@ class EmergencyCall:
     Each instance corresponds to one row of data in police_data.csv with the irrelevant columns removed
 
     Attributes:
-        - date: the date of the call
-        - emergency: the reason for the call
-        - location: the location of the incident
+      - date: the date of the call
+      - emergency: the reason for the call
+      - location: the location of the incident
 
     Representation Invariants:
-        - self.emergency != ''
-        - self.location != ''
+      - self.emergency != ''
+      - self.location != ''
     """
     date: datetime.date
     emergency: str
@@ -39,12 +43,10 @@ class CovidData:
         - prov_terr: the province or territory that corresponds with the covid data
 
     Representation Invaraints:
-        - self.num_confirmed >= 0
-        - self.num_deaths >= 0
-        - self.num_active >= 0
-        - self.prov_terr in {'British Columbia', 'Alberta', 'Saskatchewan', 'Manitoba', 'Ontario', 'Quebec',
-        'Newfoundland & Labrador', 'New Brunswick', 'Nova Scotia', 'Prince Edward Island', 'Northwest Territories',
-        'Nunavut', 'Yukon'}
+      - self.num_confirmed >= 0
+      - self.num_deaths >= 0
+      - self.num_active >= 0
+      - self.prov_terr in PROV_AND_TERR
     """
     date: datetime.date
     num_confirmed: int
@@ -121,15 +123,15 @@ def read_police_data(filename: str) -> list[EmergencyCall]:
 
 
 def covid_data_str_to_date(date_str: str) -> datetime.date:
-    """Convert a string in the yyyy-mm-dd format to a datetime.date
+    """Convert a string in the yyyy-mm-dd format (from the covid_data.csv file) to a datetime.date
 
     Preconditions:
-        - len(date_str) == 10
-        - date_str[4] == '-'
-        - date_str[7] == '-'
-        - int(date_str[:4]) >= 0
-        - 0 <= int(date_str[5:7]) <= 12
-        - 0 <= int(date_str[8:]) <= 31
+      - len(date_str) == 10
+      - date_str[4] == '-'
+      - date_str[7] == '-'
+      - int(date_str[:4]) >= 0
+      - 0 <= int(date_str[5:7]) <= 12
+      - 0 <= int(date_str[8:]) <= 31
 
     >>> covid_data_str_to_date('2020-01-01')
     datetime.date(2020, 1, 1)
@@ -139,25 +141,54 @@ def covid_data_str_to_date(date_str: str) -> datetime.date:
 
 
 def police_data_str_to_date(date_str: str) -> datetime.date:
-    """Convert a string in the yyyy-mm format to a datetime.date with the day
+    """Convert a string in the yyyy-mm format (from the police_data.csv file) to a datetime.date with the day
     being the last day of the corresponding month.
 
     Preconditions:
-        - len(date_str) == 7
-        - date_str[4] == '-'
-        - int(date_str[:4]) >= 0
-        - 0 <= int(date_str[5:]) <= 12
+      - len(date_str) == 7
+      - date_str[4] == '-'
+      - int(date_str[:4]) >= 0
+      - 0 <= int(date_str[5:]) <= 12
 
     >>> covid_data_str_to_date('2020-01')
     datetime.date(2020, 1, 31)
-    >>> covid_data_str_to_date('1999-12')
+    >>> covid_data_str_to_date('1999-02')
     datetime.date(1999, 2, 28)
     """
 
 
-def filter_physical_crimes(data: list[list]) -> list[list]:
-    """Remove the row entries in data that do not correspond to physical crimes
+def filter_physical_crimes(data: list[EmergencyCall]) -> list[EmergencyCall]:
+    """Return a new list of EmergencyCall with only physical crimes.
 
     Preconditions:
-        - len(data) != 0
+      - len(data) != 0
+
+    >>> call1 = EmergencyCall(datetime.date(2020, 1, 31), 'assault', 'Fake Street E')
+    >>> call2 = EmergencyCall(datetime.date(2020, 2, 29), 'suicide', 'Fake Street W')
+    >>> filter_physical_crimes([call1, call2])
+    [call1]
+    """
+
+
+def filter_by_covid_case_per_month(data: list[CovidData], prov_terr: str, month: int, year: int) -> list[CovidData]:
+    """Return a new list of CovidData with only CovidData corresponding to month, year for prov_terr
+
+    Preconditions:
+      - len(data) != 0
+      - year >= 0
+      - 1 <= month <= 12
+      - prov_terr in PROV_AND_TERR
+    """
+
+
+def calc_covid_case_per_month(data: list[CovidData], prov_terr: str, month: int, year: int) -> tuple[int, int]:
+    """Return a tuple of the total number of new confirmed covid cases in month, year, represented as an int,
+    for the given prov_terr and the total number of new covid related deaths in month, represented as an int, for the
+    given prov_terr
+
+    Preconditions:
+      - len(data) != 0
+      - year >= 0
+      - 1 <= month <= 12
+      - prov_terr in PROV_AND_TERR
     """
