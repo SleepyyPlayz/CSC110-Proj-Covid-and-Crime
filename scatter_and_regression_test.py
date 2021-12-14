4fac0d69f5ffea36e8ad07112c67ff7a3614ce42
SleepyyPlayz <16306416+SleepyyPlayz@users.noreply.github.com>
2021-12-14 02:26:02 -0500

    Update scatter_and_regression_test.py

--- scatter_and_regression_test.py
+++ scatter_and_regression_test.py
@@ -12,15 +12,9 @@ This file is Copyright (c) 2021 Nicholas Poon, Raghav Srinivasan, Khushil Nagda,
 import dataframes as df
 import matplotlib.pyplot as plt
 
-# Covid Cases Graph for Canada for 2020 and 2021
-# date_20_21 = df.covid_data_canada_20_21_df['Date']
 cases_20_21 = df.covid_data_canada_20_21_df['Number of New Active Cases in Canada']
 
-# physical_covid_canada_20_21 = plt.figure()
-# covid_canada_20_21_axes = physical_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
-# covid_canada_20_21_axes.plot(date_20_21, cases_20_21)
-
-# Total PHYSICAL Crimes Graph for Canada for 2020 and 2021
+# ----------------- Total PHYSICAL Crimes in relation to COVID Analysis -------------------- #
 physical_covid_canada_20_21 = plt.figure()
 physical_covid_canada_20_21_axes = physical_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
 physical_crimes_20_21 = df.total_police_data_canada_20_21_physical_df['Total Physical Crimes in Canada']
@@ -28,7 +22,7 @@ physical_crimes_20_21 = df.total_police_data_canada_20_21_physical_df['Total Phy
 physical_covid_canada_20_21_axes.scatter(cases_20_21, physical_crimes_20_21.iloc[0:20])
 physical_covid_canada_20_21_axes.set_title("Covid -> Total Physical Crime")
 
-# Total NON PHYSICAL Crimes Graph for Canada for 2020 and 2021
+# ------------------ Total NON PHYSICAL Crimes in relation to COVID Analysis ---------------- #
 non_physical_covid_canada_20_21 = plt.figure()
 non_physical_covid_canada_20_21_axes = non_physical_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
 non_physical_crimes_20_21 = \
@@ -37,7 +31,7 @@ non_physical_crimes_20_21 = \
 non_physical_covid_canada_20_21_axes.scatter(cases_20_21, non_physical_crimes_20_21.iloc[0:20])
 non_physical_covid_canada_20_21_axes.set_title("Covid -> Total Non-Physical Crime")
 
-# Total PUBLIC Crimes Graph for Canada for 2020 and 2021
+# ------------------ Total PUBLIC Crimes in relation to COVID Analysis --------------------- #
 public_covid_canada_20_21 = plt.figure()
 public_covid_canada_20_21_axes = public_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
 public_crimes_20_21 = \
@@ -46,7 +40,7 @@ public_crimes_20_21 = \
 public_covid_canada_20_21_axes.scatter(cases_20_21, public_crimes_20_21.iloc[0:20])
 public_covid_canada_20_21_axes.set_title("Covid -> Total Public Crime")
 
-# Total PRIVATE Crimes Graph for Canada for 2020 and 2021
+# ------------------ Total PRIVATE Crimes in relation to COVID Analysis -------------------- #
 private_covid_canada_20_21 = plt.figure()
 private_covid_canada_20_21_axes = private_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
 private_crimes_20_21 = \
