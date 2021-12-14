e26701e50cc5583314bbf7aff2c60168ce6cd894
SleepyyPlayz <16306416+SleepyyPlayz@users.noreply.github.com>
2021-12-14 03:15:45 -0500

    linear regression added

--- scatter_and_regression_test.py
+++ scatter_and_regression_test.py
@@ -11,16 +11,29 @@ This file is Copyright (c) 2021 Nicholas Poon, Raghav Srinivasan, Khushil Nagda,
 # import tkinter as tk
 import dataframes as df
 import matplotlib.pyplot as plt
+import numpy as np
 
 cases_20_21 = df.covid_data_canada_20_21_df['Number of New Active Cases in Canada']
 
 # ----------------- Total PHYSICAL Crimes in relation to COVID Analysis -------------------- #
+
+# Graph
 physical_covid_canada_20_21 = plt.figure()
 physical_covid_canada_20_21_axes = physical_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
 physical_crimes_20_21 = df.total_police_data_canada_20_21_physical_df['Total Physical Crimes in Canada']
 # date_20_21 = df.total_police_data_canada_20_21_physical_df['Date']
 physical_covid_canada_20_21_axes.scatter(cases_20_21, physical_crimes_20_21.iloc[0:20])
-physical_covid_canada_20_21_axes.set_title("Covid -> Total Physical Crime")
+
+# Stat Analysis and Regression Plot
+physical_model = np.polyfit(cases_20_21, physical_crimes_20_21.iloc[0:20], 1)
+physical_predict = np.poly1d(physical_model)
+x_lin_reg = range(cases_20_21.min(), cases_20_21.max())
+y_lin_reg = physical_predict(x_lin_reg)
+physical_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r')
+
+physical_covid_canada_20_21_axes.set_title("Covid -> Total Physical Crime (r=" +
+                                           str(np.corrcoef(cases_20_21, physical_crimes_20_21.iloc[0:20])[0, 1]) + ')')
+
 
 # ------------------ Total NON PHYSICAL Crimes in relation to COVID Analysis ---------------- #
 non_physical_covid_canada_20_21 = plt.figure()
